7e07fd1c7ae010b3b81cebdee220c694aa18f9b8
Paul Massey <paul.massey@tenforce.com>
2016-02-04 15:18:45 +0100

    First version of the installer for the front-end (assumption is that this is following the same route as unifiedviews installer).

---
+++ install_frontend.sh
@@ -0,0 +1,234 @@
+#!/bin/bash
+########################################################################
+# Author  : P.A.Massey
+# Date    : 02/02/2016
+# Purpose : Installer for the CODI frontend
+
+INSTALLDIR=`pwd`
+
+########################################################################
+
+: ${STAGE:="DOWNLOAD_BUILD"}
+
+   # General environment variables which can be over-ridden
+
+: ${MAVEN_OPTS:="-Xms256m -Xmx1024m -XX:PermSize=256m"}
+: ${BUILDDIR:=frontend_build}
+
+   #####################################################################
+   # Assume connected to the Internet and that files can be downloaded
+   # as needed.
+
+MAVEN_REPO_LOCAL=${INSTALLDIR}/downloads/repository
+MAVEN_OFFLINE=
+if [ "${DOWNLOAD_ALLOWED}" = "no" -o "${STAGE}" = "INSTALL_BUILT" ] ; then
+    MAVEN_OFFLINE="-o"
+fi
+
+   #####################################################################
+
+export MAVEN_OPTS
+
+#####################################################################
+
+JAVA_VERSION=1.7.0
+MAVEN_VERSION=3.3.9
+TOMCAT_VERSION=7.0.67
+
+########################################################################
+# Test that everything is installed as expected.
+
+check_installed() {
+    for i in $*
+    do
+	if ! hash $i 2>/dev/null; then
+	    echo "ERROR: $i is not installed"
+	    exit -1;
+	fi
+    done
+}
+
+########################################################################
+# Install some standard stuff
+
+install_editor() {
+    if ! hash emacs 2>/dev/null; then
+	yum -y install emacs vim
+    fi
+}
+
+install_java() {
+    if [ -f "downloads/jdk-7u80-linux-x64.rpm" ]
+    then
+	rpm -Uvh downloads/jdk-7u80-linux-x64.rpm
+	echo "export JAVA_HOME=/usr/java/jdk1.7.0_80/jre" >> /etc/environment
+	echo "export JRE_HOME=/usr/java/jdk1.7.0_80/jre" >> /etc/environment	
+    elif [ -d "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64" ]
+    then
+	echo "*** INFO: java version installed okay"
+    else
+	# Assume an update will be required.
+	yum -y install java-${JAVA_VERSION}-openjdk-devel
+	# Add to path setting
+	echo "export JAVA_HOME=\"/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64\"" >> ${HOME}/.bashrc
+	echo "export JRE_HOME=\"/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64\"" >> ${HOME}/.bashrc	
+	echo "export JAVA_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64" >> /etc/environment
+	echo "export JRE_HOME=/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64" >> /etc/environment	
+        if  [ ! -d "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk.x86_64" ]
+	then
+	    echo "*** ERROR: failed to setup Java - download from Oracle"
+1	    exit -1;
+	fi
+    fi
+}
+
+install_maven3() {
+    if [ ! -d "/opt/apache-maven-${MAVEN_VERSION}" ]
+    then
+	pushd ${INSTALLDIR}/downloads
+	  tar xvf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+	  cp -r apache-maven-${MAVEN_VERSION} /opt
+	  # Add to path setting
+	  echo "export PATH=\"/opt/apache-maven-${MAVEN_VERSION}/bin:$PATH\"" >> ${HOME}/.bashrc
+        popd
+    fi
+    echo "/opt/apache-maven-${MAVEN_VERSION}/bin"
+}
+
+install_tomcat7() {
+    if [ ! -d "/usr/local/tomcat7" ]
+    then
+	pushd /usr/local
+	  tar xvf ${INSTALLDIR}/downloads/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+	  mv apache-tomcat-${TOMCAT_VERSION} tomcat7
+	  cp ${INSTALLDIR}/config-files/tomcat-users.xml /usr/local/tomcat7/conf
+	popd
+    fi
+}
+
+########################################################################
+# 
+install_basics() {
+    yum -y update
+    yum -y install git gcc make autoconf net-tools automake libtool \
+	flex openssl bison gperf gawk m4 make openssl-devel readline-devel \
+	wget rpm-build redhat-rpm-config
+}
+
+install_virtuoso_sesame() {
+    pushd ${INSTALLDIR}/downloads
+      mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} ${MAVEN_OFFLINE} \
+          install:install-file -Dfile=virt_sesame2.jar \
+	  -DgroupId=com.openlinksw -DartifactId=virtuoso7-sesame -Dversion=2.6.10.BUILD1.12\
+	  -Dpackaging=jar
+    popd 
+}
+
+########################################################################
+
+build_council() {
+    pushd ${INSTALLDIR}/downloads/council-rdf-templating
+      mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} ${MAVEN_OFFLINE} ${MAVEN_PROFILE} \
+	  clean install
+    popd
+}
+
+build_virtuoso() {
+    check_installed automake libtool
+    
+    pushd ${INSTALLDIR}/downloads
+     git clone https://github.com/nvdk/virtuoso-rpm-builder.git
+     useradd rpmbuild
+     mkdir -p /home/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+     echo '%_topdir /home/rpmbuild' > /home/rpmbuild/.rpmmacros
+     git clone https://github.com/openlink/virtuoso-opensource.git /home/rpmbuild/virtuoso-opensource
+     cp SPECS/virtuoso-opensource.spec /home/rpmbuild/SPECS/
+     cp build-rpm.sh /home/rpmbuild/
+     cp tf-addons/ /home/rpmbuild/virtuoso-opensource/tf-addons
+     chown rpmbuild -R /home/rpmbuild
+     chmod +x /home/rpmbuild/build-rpm.sh
+     su rpmbuild -l -c "export VIRT_BRANCH && /home/rpmbuild/build-rpm.sh"
+    popd
+}
+
+########################################################################
+
+save_built() {
+    mkdir -p ${INSTALLDIR}/${BUILDDIR}
+    pushd ${INSTALLDIR}/${BUILDDIR}
+      cp ${INSTALLDIR}/*.sh .
+      cp ${INSTALLDIR}/*.org .
+      cp ${INSTALLDIR}/*.html .          
+      cp -r ${INSTALLDIR}/config-files .
+      cp -r ${INSTALLDIR}/downloads .
+      rm -rf downloads/repository
+      rm -rf downloads/Core*
+      rm -rf downloads/UV*
+      rm -rf downloads/docker-*
+    popd
+    pushd ${INSTALLDIR}
+     tar cvf ${BUILDDIR}.tar ${BUILDDIR}
+     gzip -9 ${BUILDDIR}.tar
+    popd
+}
+
+########################################################################
+
+install_saved() {
+    pushd ${INSTALLDIR}
+      mkdir -p /usr/local/tomcat7/lib    
+      mkdir -p /usr/local/tomcat7/bin
+      mkdir -p /usr/local/tomcat7/webapps
+      cp *.war /usr/local/tomcat7/webapps
+      cp downloads/virtjdbc4.jar /usr/local/tomcat7/lib
+      if [ ! -f "/etc/init.d/tomcat7" ] ; then
+	cp ${INSTALLDIR}/config-files/tomcat.service /etc/init.d/tomcat7
+	cp ${INSTALLDIR}/config-files/tomcat-setenv.sh /usr/local/tomcat7/bin/setenv.sh
+	echo >> /etc/default/tomcat7
+	sed -i "s/^TOMCAT7_USER.*/TOMCAT7_USER=root/" /etc/default/tomcat7
+	sed -i "s/^TOMCAT7_GROUP.*/TOMCAT7_GROUP=root/" /etc/default/tomcat7
+	groupadd tomcat7
+	useradd -s /bin/bash -g tomcat7 tomcat7
+	chown -Rf tomcat7.tomcat7 /usr/local/tomcat7/  
+	chmod +x /etc/init.d/tomcat7
+	chkconfig --add tomcat7
+	chkconfig tomcat7 on
+      fi
+      service tomcat7 start
+    popd
+}
+
+########################################################################
+#
+########################################################################
+
+install_editor
+install_maven3
+install_java
+install_tomcat7
+install_basics
+
+source ~/.bashrc
+
+check_installed java mvn git gcc gmake autoconf chkconfig
+
+case "${STAGE}" in
+    DOWNLOAD_BUILD)
+	install_virtuoso_sesame
+	build_council
+	# build_virtuoso
+       ;;
+    SAVE_BUILT)
+	save_built
+       ;;
+    INSTALL_BUILT)
+	install_saved
+       ;;
+    *)
+	echo "*** ERROR: Options are ..."
+	exit -1;
+esac
+
+###############################################################################
+exit 0;
+###############################################################################
